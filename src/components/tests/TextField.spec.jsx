3a03a4c2f6c03f33e12f574d8a9f45c24a2274d1
james <james@irm.kr>
2024-09-21 20:45:57 +0900

    [fix] describe, it, test unit test basic

--- src/components/tests/TextField.spec.jsx
+++ src/components/tests/TextField.spec.jsx
@@ -24,7 +24,38 @@ it('className props으로 설정한 css class가 적용된다.', async () => {
 
   // className 이란 내부 prop이나 state 값을 검증하는 것이 아니라,
   // 렌더링되는 DOM 구조가 올바르게 변경되었는지 확인해야 한다. 최종적으로 사용자가 보는 결과는 DOM이기 때문.
-  expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
-    'my-class',
-  );
+  // expect(screen.getByPlaceholderText('텍스트를 입력해 주세요.')).toHaveClass(
+  //   'my-class',
+  // );
+
+  const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+  screen.debug();
+
+  expect(textInput).toHaveClass('my-class');
+});
+/*
+// it -> test 함수의 alias 이다. 기능상의 차이는 없다.
+// it(should ~~~~)
+// test(if~~~~~)
+// describe 함수는 테스트를 그룹화해서 새로운 블럭, 즉 컨텍스트를 만드는 역할을 한다. 원하는 테스트들을 묶어서 최상위가 아닌 독립된 컨텍스트를 만들어 그룹화 가능.
+ */
+
+describe('placeholder', () => {
+  test('기본 placeholder "텍스트를 입력해 주세요"가 노출된다.', async () => {
+    // it 함수 내부에는 기대 결과 코드를 작성한다.
+    await render(<TextField />);
+    const textInput = screen.getByPlaceholderText('텍스트를 입력해 주세요.');
+    screen.debug();
+    expect(textInput).toBeInTheDocument();
+    //단언 = assertion : 테스트가 통과하기 위한 조건 서술. -> 검증 실행.
+  });
+
+  //TextField 의 Prop 검증
+  it('placeholder props에 따라 placehoolder가 변경된다.', async () => {
+    await render(<TextField placeholder={'상품명을 입력해 주세요.'} />);
+
+    const textInput = screen.getByPlaceholderText('상품명을 입력해 주세요.');
+
+    expect(textInput).toBeInTheDocument();
+  });
 });
