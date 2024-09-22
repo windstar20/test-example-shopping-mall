2769db4fb1a1eca7d793e02bfc77dbc4be0e86e6
james <james@irm.kr>
2024-09-22 19:02:49 +0900

    [fix] 모듈 모킹. [body] 가짜 모듈이 실제 모듈을 대체하는 것. 그리고 모킹 초기화 - 테스트의 독립성과 안정성을 위한 작업.

--- src/pages/cart/components/tests/EmptyNotice.spec.jsx
+++ src/pages/cart/components/tests/EmptyNotice.spec.jsx
@@ -4,4 +4,22 @@ import React from 'react';
 import EmptyNotice from '@/pages/cart/components/EmptyNotice';
 import render from '@/utils/test/render';
 
-it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate함수가 호출된다', async () => {});
+// useNavigate 훅으로 반환받은 navigate 함수가 올바르게 호출되었는가. - 스파이 함수.
+// useNavigate 자체를 spy 함수로 모킹하여 React router dom 모듈의 기존 구현을 대체함.
+const navigateFn = vi.fn();
+
+//* vi.mock API
+//? 실제 모듈을 모킹한 모듈로 대체하여 테스트 실행.
+//? 실행할 때 임포트 된 모듈을 가져오기 전에 실행된다.
+vi.mock('react-router-dom', async () => {
+  // 한편 우리가 모킹하고 싶은 것은 react 라우터의 모든 구현이 아니라 useNavigate hook만 모킹하는 것이다.
+  // 이와 같이 일부 모듈만 모킹하고, 나머지는 기존의 기능을 사용하는 경우, vi.importActual 함수를 사용하여 진행할 수 있다.
+
+  const original = await vi.importActual('react-router-dom');
+  return { ...original, useNavigate: () => navigateFn };
+});
+it('"홈으로 가기" 링크를 클릭할경우 "/"경로로 navigate 함수가 호출된다', async () => {
+  const { user } = await render(<EmptyNotice />);
+  await user.click(screen.getByText('홈으로 가기'));
+  expect(navigateFn).toHaveBeenNthCalledWith(1, '/');
+});
--- src/utils/test/setupTests.js
+++ src/utils/test/setupTests.js
@@ -6,16 +6,26 @@ import { handlers } from '@/__mocks__/handlers';
 /* msw */
 export const server = setupServer(...handlers);
 
+//* 공통 설정 파일
+
 beforeAll(() => {
   server.listen();
 });
 
 afterEach(() => {
   server.resetHandlers();
+
+  // 모킹된 모의 객체 호출에 대한 히스토리를 초기화
+  // 모킹된 모듈의 구현을 초기화하지는 않는다. -> 모킹된 상태로 유지됨
+  // -> 모킹 모듈 기반으로 작성한 테스트가 올바르게 실행
+  // 반면, 모킹 히스토리가 계속 쌓임(호출 횟수나 인자가 계속 변경) -> 다른 테스트에 영향을 줄 수 있음.
+  // 그렇기 때문에 테스트 실행이 끝날때마다 clearAllMocks 함수를 호출하여 히스토리를 초기화하고 있다.
   vi.clearAllMocks();
 });
 
 afterAll(() => {
+  // 모든 테스트가 종료된 후 모킹 모듈이 더 이상 의미가 없기 때문에 vi.resetAllMock 함수를 호출하여
+  // spy 함수에 호출했으나 결과뿐만 아니라 mocking 모듈에 대한 모든 구현을 초기화한다.
   vi.resetAllMocks();
   server.close();
 });
