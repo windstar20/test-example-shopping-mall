9aec87b6b1688f415ac7c258c17d94f1bc4abc32
박현민 <ethnos@james.local>
2024-09-27 01:20:43 +0900

    [fix] zustand mocing 테스트 코드 작성

--- src/pages/cart/components/tests/ProductInfoTable.spec.jsx
+++ src/pages/cart/components/tests/ProductInfoTable.spec.jsx
@@ -10,11 +10,57 @@ import render from '@/utils/test/render';
 
 beforeEach(() => {
   mockUseUserStore({ user: { id: 10 } });
-
-})
+  mockUseCartStore({
+    cart: {
+      6: {
+        id: 6,
+        title: 'Handmade Cotton Fish',
+        price: 809,
+        description:
+          'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+        images: [
+          'https://user-images.githubusercontent.com/35371660/230712070-afa23da8-1bda-4cc4-9a59-50a263ee629f.png',
+          'https://user-images.githubusercontent.com/35371660/230711992-01a1a621-cb3d-44a7-b499-20e8d0e1a4bc.png',
+          'https://user-images.githubusercontent.com/35371660/230712056-2c468ef4-45c9-4bad-b379-a9a19d9b79a9.png',
+        ],
+        count: 3,
+      },
+      7: {
+        id: 7,
+        title: 'Awesome Concrete Shirt',
+        price: 442,
+        description:
+          'The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J',
+        images: [
+          'https://user-images.githubusercontent.com/35371660/230762100-b119d836-3c5b-4980-9846-b7d32ea4a08f.png',
+          'https://user-images.githubusercontent.com/35371660/230762118-46d965ab-7ea8-4e8a-9c0f-3ed90f96e1cd.png',
+          'https://user-images.githubusercontent.com/35371660/230762139-002578da-092d-4f34-8cae-2cf3b0dfabe9.png',
+        ],
+        count: 4,
+      },
+    },
+  });
+});
 
 it('장바구니에 포함된 아이템들의 이름, 수량, 합계가 제대로 노출된다', async () => {
+  await render(<ProductInfoTable />);
 
+  //상품데이터는 테이블 형태이므로 각 테이블에 해당하는 요소를 조회해야함.
+  //이때 테이블의 행은 row 라는 role 로 조회할 수 있다.
+  const [firstItem, secondItem] = screen.getAllByRole('row');
+  //이제 각 테이블 행별로 설정한 상품 mock 데이터가 잘 렌더링 되는지만 확인하면 테스트 완료됨.
+  //firstItem 을 대상으로 상품명이나 가격 정보가 잘 나오는지 확인해야 한다.
+  //조회한 요소를 기준으로 react testing library query 를 사용하고 싶다면 widthin 함수를 사용함.
+  expect(
+    within(firstItem).getByText('Handmade Cotton Fish'),
+  ).toBeInTheDocument();
+  expect(within(firstItem).getByRole('textbox')).toHaveValue('3');
+  expect(within(firstItem).getByText('$2,427.00')).toBeInTheDocument();
+  expect(
+    within(secondItem).getByText('Awesome Concrete Shirt'),
+  ).toBeInTheDocument();
+  expect(within(secondItem).getByRole('textbox')).toHaveValue('4');
+  expect(within(secondItem).getByText('$1,768.00')).toBeInTheDocument();
 });
 
 it('특정 아이템의 수량이 변경되었을 때 값이 재계산되어 올바르게 업데이트 된다', async () => {});
